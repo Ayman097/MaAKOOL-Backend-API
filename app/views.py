a1f4ce27ceba2910c3188832127546d823f0a418
Mostafa Nabieh <engaymanfarag1997@gmail.com>
2023-11-05 14:18:24 +0200

    adding serializers and create product detail

--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view()
 def product_list(request):
-    return Response('OK')
+    products = Product.objects.all()
+    #print(products)
+    serializer = ProductSerializer(products, many=True)
+    print(serializer.data)
+    
+    return Response(serializer.data)
+@api_view()
+def product_detail(request, id):
+
+    product = get_object_or_404(Product, pk=id)
+    print(product, product.price, product.description)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+    
+    
