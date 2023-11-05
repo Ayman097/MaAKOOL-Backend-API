ae478cd2ed4d3cca87c31b87ed2194f84bc8903e
Mostafa Nabieh <engaymanfarag1997@gmail.com>
2023-11-05 21:06:27 +0200

    create account app and build AuthSystem

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'app',
+    'account',
     
 ]
 
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('resturant/', include('app.urls'))
+    path('resturant/', include('app.urls')),
+    path('account/', include('account.urls')),
     # path('api-auth/', include('rest_framework.urls'))
 ]
 
