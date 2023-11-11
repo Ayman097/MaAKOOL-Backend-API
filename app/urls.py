b1360b7af742f204cefaaed2f1a6357ff83bb70b
Mostafa Nabieh <engaymanfarag1997@gmail.com>
2023-11-12 01:06:26 +0200

    Create CRUD Operation For Products

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('products/<int:id>', views.product_detail, name='product'),
     path('category/', views.category_list, name='category'),
     path('category-products/<int:category_id>/', views.category_product_list, name='category-products'),
+
+    # Admin or Staff only
+    path('products/new',  views.new_product, name='new_product'),
+    path('products/edit/<int:id>',  views.edit_product, name='edit_product'),
+    path('products/delete/<int:id>',  views.delete_product, name='delete_product'),
     
 ]
