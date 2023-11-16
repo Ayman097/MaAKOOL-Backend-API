f630fd81ca2409c0a54c491e038289390ff3bbe0
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2023-11-16 06:09:29 +0200

    before profile edit section

--- order/api/views.py
+++ order/api/views.py
@@ -1,4 +1,4 @@
-# views.py
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,6 +9,7 @@ from .serializers import OrderItemsSerializer, DetailedOrderSerializer, OrderSer
 from accounts.models import User
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 
 from rest_framework.exceptions import AuthenticationFailed
