efc9415f192d599ca313e782e3de668c043bbd40
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-11-17 02:44:02 +0200

    clean master

--- order/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Order, OrderItems
-
-admin.site.register(Order)
-admin.site.register(OrderItems)
--- project/env.py
+++
@@ -1,5 +0,0 @@
-# import os
-
-
-# EMAIL_USER = 'a7med74yaso@gmail.com'
-# EMAIL_PASS = 'izsy isct nqpu evub'
