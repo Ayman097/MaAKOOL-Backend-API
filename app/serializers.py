2dde77c0a0b8c03adf1295136c395d7d6c46e9e1
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-11-25 07:33:42 +0200

    modify model

--- app/serializers.py
+++ app/serializers.py
@@ -4,7 +4,6 @@ from .models import *
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.ReadOnlyField(source='category.name')
 
-
     class Meta:
         model = Product
         fields = '__all__'
