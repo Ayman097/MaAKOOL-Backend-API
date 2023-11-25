1df86df57a6f27a0f225d2866e6f113471ae76c8
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2023-11-26 01:09:05 +0200

    preparing for final merging

--- accounts/models.py
+++ accounts/models.py
@@ -72,23 +72,6 @@ class ContactUsModel(models.Model):
         return f"{self.name}"
 
 
-# class Rating(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     rating = models.IntegerField(
-#         validators=[MaxValueValidator(5), MinValueValidator(1)]
-#     )
-
-#     class Meta:
-#         unique_together = (
-#             "user",
-#             "product",
-#         )
-
-#     def __str__(self):
-#         return f"{self.user.username} - {self.product.name} - {self.rating}"
-
-
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
